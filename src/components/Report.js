bf97cdeaf9d5f7d47e2894706c2422aae2cee40c
unknown <amyyu116@gmail.com>
2024-07-11 23:56:18 -0400

    fixed?

--- src/components/Report.js
+++ src/components/Report.js
@@ -17,10 +17,7 @@ const Report = (props) => {
             try {
                 setReport(null);
                 console.log('Fetching report...');
-                const response = await axiosInstance.post(`/report/${props.prolificID}`, {
-                    prolificID: props.prolificID,
-                    model: props.model
-                });
+
                 setReport(response.data);
                 setError(null);
             } catch (error) {
